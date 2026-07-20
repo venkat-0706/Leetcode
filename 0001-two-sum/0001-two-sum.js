531ef6ba68a1c0b202d94c40d4edd6af40270103
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2026-07-20 22:57:01 +0530

    Time: 3 ms (59.51%), Space: 57.8 MB (5.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,15 +1,22 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
 var twoSum = function(nums, target) {
-    let n = nums.length;
-    for(let i=0;i<n;i++){
-        for(let j=i+1;j<n;j++){
-            if(nums[i]+ nums[j] == target){
-                return [i,j];
-            }
+    // Map elements to objects containing value and original index
+    let indexedNums = nums.map((num, index) => ({ value: num, index: index }));
+    
+    // Fix: Sort numerically using a comparator function
+    indexedNums.sort((a, b) => a.value - b.value);
+    
+    let i = 0;
+    let j = indexedNums.length - 1;
+    
+    while (i < j) { // Changed to i < j since elements cannot pair with themselves
+        let sum = indexedNums[i].value + indexedNums[j].value;
+        
+        if (sum === target) {
+            return [indexedNums[i].index, indexedNums[j].index]; // Return original indices
+        } else if (sum < target) {
+            i++;
+        } else {
+            j--;
         }
     }
 };
