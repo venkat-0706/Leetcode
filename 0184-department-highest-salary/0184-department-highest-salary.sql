bacaa54a85402d39424e8d80437b83179706e92b
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2026-07-26 23:19:17 +0530

    Time: 617 ms (95.2%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+with cte as (
+    select d.name as 'Department',
+           e.name as 'Employee',
+           e.salary as 'Salary',
+           dense_rank() over(partition by d.id order by e.salary desc) as rnk
+           from Employee e left join Department d 
+           on e.departmentId = d.id
+)
+select Department , Employee , Salary 
+from cte 
+where rnk = 1;
