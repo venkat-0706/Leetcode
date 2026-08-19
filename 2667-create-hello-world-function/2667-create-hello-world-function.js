cd83736dcfb97e7c2449c54da4cc0dfa3d700e35
Abbireddy Venkata Chandu <119731968+venkat-0706@users.noreply.github.com>
2026-08-19 11:57:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -2,7 +2,13 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
-    return function() {
-        return "Hello World";
-    };
+    
+    return function(...args) {
+        
+    }
 };
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
